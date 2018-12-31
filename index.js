2436f06d5bd549c2f59a0ec1db9c7225e8ec71a9
Serena Lertkantitham <lertkantitham@gmail.com>
2018-12-31 09:25:05 +0800

    github api response working

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+function displayRepos(){
+    let user = $('form').find('#username').val();
+    let url = `https://api.github.com/users/${user}/repos`;
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText)
+        })
+        .then(responseJson => console.log(responseJson))
+        .catch(err => console.log(err.message));
+}
+
 function watchForm(){
     $('form').submit(event => {
         event.preventDefault();
-        console.log('hello');
+        displayRepos();
     })
 }
+
 watchForm();
